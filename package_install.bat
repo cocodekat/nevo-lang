4d4d41eabaedc4744597b46077b0f65da17b5c48
cocodekat <philip.gaina@icloud.com>
2025-11-06 22:09:18 +0100

    Update package_install.bat

--- package_install.bat
+++ package_install.bat
@@ -64,7 +64,8 @@ echo ✅ Using Python: %PYTHON_BIN%
 echo @echo off
 echo SETLOCAL
 echo set "BASE_DIR=%%~dp0"
-echo if exist "%%BASE_DIR%%python\python.exe" (set "PYTHON_BIN=%%BASE_DIR%%python\python.exe") else (set "PYTHON_BIN=python")
+echo set "PYTHON_BIN=python"
+echo if exist "%%BASE_DIR%%python\python.exe" set "PYTHON_BIN=%%BASE_DIR%%python\python.exe"
 echo set "PACKAGE_PY=%%BASE_DIR%%package.py"
 echo if not exist "!PACKAGE_PY!" (
 echo     echo ❌ package.py not found!
@@ -72,6 +73,7 @@ echo     exit /b 1
 echo )
 echo "!PYTHON_BIN!" "!PACKAGE_PY!" %%*
 ) > "%INSTALL_DIR%\package.bat"
+
 
 
 echo ✅ Installation complete!
